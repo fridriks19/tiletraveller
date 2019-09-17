14f6e8d243be9931b6b2ac2b1a713397188b0630
Fridrik Steingrims <fridriksteingrims@Fridriks-MacBook-Pro.local>
2019-09-17 09:40:42 +0000

    Tiletraveller test case 1

---
+++ TileTraveller.py
@@ -0,0 +1,13 @@
+#algorithm
+#test
+
+
+
+
+
+
+
+print ("You can travel:" , ''''þær leiðir sem eru eftir'''')
+print ("Direction:", input_nswe)
+print ("Not a valid direction!")
+print ("Victory!"))
